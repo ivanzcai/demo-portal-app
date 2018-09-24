8c6a859bc658191a24242ad4a1f6f1e32336f7ca
zcai2672 <ivan.z.cai@gmail.com>
2018-09-24 16:40:22 +1000

    rename folder

---
+++ Consultant-Portal-App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Consultant_Portal_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
