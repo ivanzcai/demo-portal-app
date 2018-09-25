2394d4ca067055667e87e9a5974661d04047f29d
zcai2672 <ivan.z.cai@gmail.com>
2018-09-25 11:41:47 +1000

    added Unit Test Project

---
+++ Demo-Portal-App-Test/HomeControllerTest.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+
+namespace Demo_Portal_App_Test
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void TestTrue()
+        {
+            Assert.True(true);
+        }
+        [Fact]
+        public void TestFalse()
+        {
+            Assert.False(false);
+        }
+
+    }
+}
