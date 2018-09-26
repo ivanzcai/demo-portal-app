b9f9b1c7eea4099b81d700b427f98e65c8bc21ba
Ivan Cai <ivan.z.cai@gmail.com>
2018-09-26 14:27:19 +1000

    added new integration test project

---
+++ Demo-Portal-Integration-Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Demo-Portal-Integration-Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Demo-Portal-Integration-Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7fea8094-c4e8-460e-b0b4-d6905e256f67")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Demo-Portal-Integration-Tests/UnitTest1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Demo_Portal_Integration_Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver driver = new ChromeDriver();
+            // IWebDriver driver = new ChromeDriver(Environment.GetEnvironmentVariable("ChromeWebDriver"));
+
+
+            //Navigate to a URL in browser
+            driver.Navigate().GoToUrl("http://consultant-portal-app-test.azurewebsites.net");
+
+            //Maximize the opened browser 
+            driver.Manage().Window.Maximize();
+
+            //Finds a Link with text "Run your app" and clicks on it.
+            driver.FindElement(By.LinkText("Run your app")).Click();
+            try
+            {
+                //Assert if the browser is on the correct page ,through checking Page Title
+                Assert.IsTrue(driver.Title.Contains("Use multiple environments in ASP.NET Core | Microsoft Docs"), "Page Title Not Verified");
+            }
+            catch (Exception e)
+            {
+                //Closes Browser and throws exception if assertion fails.
+                driver.Close();
+                driver.Quit();
+                throw e;
+
+
+            }
+
+            //closes browser if test passes
+            driver.Close();
+            driver.Quit();
+
+        }
+    }
+}
