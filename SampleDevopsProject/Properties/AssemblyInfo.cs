4a11be7a283aaac9695080799ae14630b111ec71
Basant.Mishra <Basant.Mishra@smsmicrosoft.com>
2018-09-26 22:17:04 +1000

    First commit

---
+++ SampleDevopsProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SampleDevopsProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SMS Management and Technology")]
+[assembly: AssemblyProduct("SampleDevopsProject")]
+[assembly: AssemblyCopyright("Copyright © SMS Management and Technology 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7ba0229c-1131-4682-8a8f-5db43e0ac21b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SampleDevopsProject/SampleTestClass.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SampleDevopsProject
+{
+    [TestClass]
+    public class SampleTestClass
+    {
+        [TestMethod]
+        static void Main(String[] ARGS)
+        {
+
+            
+            IWebDriver driver = new ChromeDriver(Environment.GetEnvironmentVariable("ChromeWebDriver"));
+
+            //Navigate to a URL in browser
+            driver.Navigate().GoToUrl("http://consultant-portal-app-test.azurewebsites.net");
+
+            //Maximize the opened browser 
+            driver.Manage().Window.Maximize();
+
+            //Finds a Link with text "Run your app" and clicks on it.
+            driver.FindElement(By.LinkText("Run your app")).Click();
+            try
+            {
+                //Assert if the browser is on the correct page ,through checking Page Title
+                Assert.IsTrue(driver.Title.Contains("Use multiple environments in ASP.NET Core | Microsoft Docs"), "Page Title Not Verified");
+            }
+            catch (Exception e)
+            {
+                //Closes Browser and throws exception if assertion fails.
+                driver.Close();
+                driver.Quit();
+                throw e;
+                
+
+            }
+
+            //closes browser if test passes
+            driver.Close();
+            driver.Quit();
+
+        }
+            }
+}
